17439d9d422782cae765925d98b4b6630810ed47
yykdev <assavictory@gmail.com>
2020-04-19 22:42:07 +0900

    User 모델 필드 추가

--- django-app/db/models/users.py
+++ django-app/db/models/users.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from utils import Constant
+
 __all__ = (
     'User',
 )
@@ -8,6 +10,19 @@ __all__ = (
 
 class User(AbstractUser):
 
+    """ Custom User Model """
+
+    avatar = models.ImageField(
+        null=True,
+        blank=True,
+    )
+    gender = models.CharField(
+        choices=Constant.GENDER_CHOICES,
+        max_length=10,
+        null=True,
+        blank=True,
+    )
     bio = models.TextField(
         default='',
+        blank=True,
     )
---
+++ django-app/utils/constant.py
@@ -0,0 +1,12 @@
+
+class Constant:
+
+    GENDER_MALE = 'male'
+    GENDER_FEMALE = 'female'
+    GENDER_OTHER = 'other'
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHER, 'Other'),
+    )
