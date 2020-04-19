af9c304abd94f7399179ef9a52b55bfa8ca236d5
yykdev <assavictory@gmail.com>
2020-04-19 23:08:28 +0900

    상속형 어드민 판넬 실습

--- django-app/db/admin.py
+++ django-app/db/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
-    list_display = ('username', 'email', 'gender', 'language', 'currency', 'is_superhost')
-    list_filter = ('language', 'currency', 'is_superhost')
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'Profile',
+            {
+                'fields': (
+                    'avatar',
+                    'gender',
+                    'bio',
+                    'birthdate',
+                    'language',
+                    'currency',
+                    'is_superhost',
+                )
+            }
+        ),
+    )
