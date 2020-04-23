07b3b8d6316c2c4d3418670f11f753c3897365ad
yykdev <assavictory@gmail.com>
2020-04-23 15:32:47 +0900

    공통 추상 모델 상속 셋팅

--- django-app/db/models/rooms.py
+++ django-app/db/models/rooms.py
@@ -1,10 +1,10 @@
-from django.db import models
+from core import models as core_models
 
 __all__ = (
     'Room',
 )
 
 
-class Room(models.Model):
+class Room(core_models.TimeStampedModel):
 
     pass
