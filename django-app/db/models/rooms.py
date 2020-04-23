1fefe5569f0f862677320eac00e6026a8fe12eb6
yykdev <assavictory@gmail.com>
2020-04-23 18:46:39 +0900

    모델별 Meta 태그 셋팅, Photo 모델 추가, 어드민 판넬 추가

--- django-app/db/models/rooms.py
+++ django-app/db/models/rooms.py
@@ -10,6 +10,7 @@ __all__ = (
     'Amenity',
     'Facility',
     'HouseRule',
+    'Photo',
 )
 
 
@@ -32,28 +33,33 @@ class RoomType(AbstractItem):
 
     """ RoomType Object Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = 'Room Types'
+        ordering = ['-created']
 
 
 class Amenity(AbstractItem):
 
     """ Amenity Object Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = 'Amenities'
 
 
 class Facility(AbstractItem):
 
     """ Facility Object Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = 'Facilities'
 
 
 class HouseRule(AbstractItem):
 
     """ HouseRule Object Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = 'House Rules'
 
 
 class Room(core_models.TimeStampedModel):
@@ -114,13 +120,32 @@ class Room(core_models.TimeStampedModel):
     )
     amenities = models.ManyToManyField(
         Amenity,
+        blank=True,
     )
     facilities = models.ManyToManyField(
         Facility,
+        blank=True,
     )
     house_rules = models.ManyToManyField(
         HouseRule,
+        blank=True,
     )
 
     def __str__(self):
         return self.name
+
+
+class Photo(core_models.TimeStampedModel):
+    """ Photo Model Definition """
+
+    caption = models.CharField(
+        max_length=80
+    )
+    file = models.ImageField()
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.caption
