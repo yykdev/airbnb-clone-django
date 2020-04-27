5a9ec0458f0237b5fe003d02d8693260675456fd
yykdev <assavictory@gmail.com>
2020-04-27 19:08:35 +0900

    관리자 패널 커스텀 예

--- django-app/db/admin/rooms.py
+++ django-app/db/admin/rooms.py
@@ -11,7 +11,101 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
-    pass
+    fieldsets = (
+        (
+            "Baseic Info",
+            {
+                # "classes": ("collapse",),
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "address",
+                    "price",
+                )
+            },
+        ),
+        (
+            "Times Info",
+            {
+                # "classes": ("collapse",),
+                "fields": (
+                    "check_in",
+                    "check_out",
+                    "instant_book",
+                )
+            },
+        ),
+        (
+            "Spaces Info",
+            {
+                # "classes": ("collapse",),
+                "fields": (
+                    "guests",
+                    "beds",
+                    "bedrooms",
+                    "baths",
+                )
+            },
+        ),
+        (
+            "More About the Spaces",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "amenities",
+                    "facilities",
+                    "house_rules",
+                )
+            },
+        ),
+        (
+            "Last Details",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "host",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+
+        "city",
+        "country",
+    )
+
+    search_fields = (
+        "city",
+        "host__username",
+    )
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
 
 
 @admin.register(models.Photo)
