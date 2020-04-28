0c0d89ddfb85829ef3b622eaf61c3c593a447619
yykdev <assavictory@gmail.com>
2020-04-28 18:47:37 +0900

    Conversation, Messages 모델 메소드 셋팅 및 어드민 판넬 셋팅

--- django-app/db/models/conversations.py
+++ django-app/db/models/conversations.py
@@ -13,10 +13,30 @@ class Conversation(core_models.TimeStampedModel):
     participants = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         blank=True,
+        related_name='conversation',
     )
 
     def __str__(self):
-        return str(self.created)
+
+        usernames = []
+
+        for user in self.participants.all():
+
+            usernames.append(user.username)
+
+        return ", ".join(usernames)
+
+    def count_messages(self):
+
+        return self.messages.count()
+
+    count_messages.short_description = "Number of Message"
+
+    def count_participants(self):
+
+        return self.participants.count()
+
+        count_participants.short_description = "Number of participants"
 
 
 class Message(core_models.TimeStampedModel):
@@ -25,10 +45,12 @@ class Message(core_models.TimeStampedModel):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        related_name='messages',
     )
     conversation = models.ForeignKey(
         Conversation,
         on_delete=models.CASCADE,
+        related_name='messages',
     )
 
     def __str__(self):
