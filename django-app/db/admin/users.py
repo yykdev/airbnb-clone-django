6c5dc79dfcffc212da0d24972c5367c54929554c
yykdev <assavictory@gmail.com>
2020-04-28 15:43:58 +0900

    users 어드민 판넬 구성 변경

--- django-app/db/admin/users.py
+++ django-app/db/admin/users.py
@@ -22,3 +22,20 @@ class CustomUserAdmin(UserAdmin):
             }
         ),
     )
+
+    list_filter = UserAdmin.list_filter + (
+        "is_superhost",
+    )
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "is_superhost",
+        "is_staff",
+        "is_superuser",
+    )
