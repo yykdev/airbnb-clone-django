4d353e1bb9621768e5dd762c58fa803630f0c22e
yykdev <assavictory@gmail.com>
2020-04-28 16:29:07 +0900

    reservations 모델 메소드 추가 및 어드민 판넬 셋팅

--- django-app/db/admin/reservations.py
+++ django-app/db/admin/reservations.py
@@ -6,3 +6,17 @@ from . import models
 class ReservationsAdmin(admin.ModelAdmin):
 
     """ ReservationsAdmin Admin Definition """
+
+    list_display = (
+        "room",
+        "status",
+        "check_in",
+        "check_out",
+        "guest",
+        "in_progress",
+        "is_finished",
+    )
+
+    list_filter = (
+        "status",
+    )
--- django-app/db/models/reservations.py
+++ django-app/db/models/reservations.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
 from core import models as core_models
 from db.models import Room
 from utils import Constant
@@ -32,3 +34,19 @@ class Reservation(core_models.TimeStampedModel):
 
     def __str__(self):
         return "{} - {}".format(self.room, self.check_in)
+
+    def in_progress(self):
+
+        now = timezone.now().date()
+
+        return self.check_in <= now < self.check_out
+
+    in_progress.boolean = True
+
+    def is_finished(self):
+
+        now = timezone.now().date()
+
+        return now > self.check_out
+
+    is_finished.boolean = True
