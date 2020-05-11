c7d0696be2416afeec59d8812f4904221c8cefc1
yykdev <assavictory@gmail.com>
2020-05-11 18:57:22 +0900

    paginator page 방식으로 리팩토링

--- django-app/rooms/views.py
+++ django-app/rooms/views.py
@@ -1,7 +1,7 @@
 from math import ceil
 
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 
 from db.models import Room
 
@@ -16,11 +16,16 @@ def all_rooms(request):
     # page, get_page 차이
     # - page 의 경우 에러를 핸들링 할 수 있고
     # - get_page 의 경우 라이브러리 내에서 except 처리가 돼 있다.
-    # rooms = paginator.page(page)
-    rooms = paginator.get_page(page)
+    # rooms = paginator.get_page(page)
 
-    context = {
-        'page': rooms,
-    }
+    try:
+        rooms = paginator.page(int(page))
 
-    return render(request, 'rooms/home.html', context=context)
+        context = {
+            'page': rooms,
+        }
+
+        return render(request, 'rooms/home.html', context=context)
+    except Exception:
+
+        return redirect('/')
