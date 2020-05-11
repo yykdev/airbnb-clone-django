f1a5c6a5cd2a847081feab00cf4b294a25163761
yykdev <assavictory@gmail.com>
2020-05-11 18:38:02 +0900

    page, get_page 차이

--- django-app/rooms/views.py
+++ django-app/rooms/views.py
@@ -12,10 +12,15 @@ def all_rooms(request):
     room_list = Room.objects.all()
 
     paginator = Paginator(room_list, 10)
+
+    # page, get_page 차이
+    # - page 의 경우 에러를 핸들링 할 수 있고
+    # - get_page 의 경우 라이브러리 내에서 except 처리가 돼 있다.
+    # rooms = paginator.page(page)
     rooms = paginator.get_page(page)
 
     context = {
-        'rooms': rooms,
+        'page': rooms,
     }
 
     return render(request, 'rooms/home.html', context=context)
