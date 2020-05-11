35362cce4254f52f64d879e4d3ca1e1401077c4e
yykdev <assavictory@gmail.com>
2020-05-11 16:52:00 +0900

    페이지네이션 기본

--- django-app/rooms/views.py
+++ django-app/rooms/views.py
@@ -1,6 +1,26 @@
+from math import ceil
+
 from django.shortcuts import render
+
+from db.models import Room
 
 
 def all_rooms(request):
 
-    return render(request, 'rooms/all_rooms.html')
+    page = request.GET.get('page', 1)
+    page = int(page or 1)
+    page_size = 5
+    limit = page * page_size
+    offset = limit - page_size
+
+    rooms = Room.objects.all()[offset:limit]
+    page_count = ceil(Room.objects.count() / page_size)
+
+    context = {
+        'rooms': rooms,
+        'page': page,
+        'page_count': page_count,
+        'page_range': range(1, page_count + 1),
+    }
+
+    return render(request, 'rooms/home.html', context=context)
