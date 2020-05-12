b19364c3f75aa0c3a1ddf687d8d6a15b859fc62d
yykdev <assavictory@gmail.com>
2020-05-12 18:19:17 +0900

    CBV , context data 리팩토링

--- django-app/rooms/views.py
+++ django-app/rooms/views.py
@@ -2,6 +2,7 @@ from math import ceil
 
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
 from django.views.generic import ListView
 
 from db.models import Room
@@ -14,3 +15,14 @@ class HomeView(ListView):
     paginate_by = 10
     # paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        now = timezone.now()
+
+        context["now"] = now
+
+        return context
