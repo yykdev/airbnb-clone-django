510c67917798667ad60430a93fe565058b50b037
yykdev <assavictory@gmail.com>
2020-05-14 00:58:21 +0900

    search 기능 구현

--- django-app/rooms/views.py
+++ django-app/rooms/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from django_countries import countries
 
-from db.models import Room, RoomType
+from db.models import Room, RoomType, Amenity, Facility
 
 
 class HomeView(ListView):
@@ -24,21 +24,44 @@ class RoomDetail(DetailView):
 
 def search(request):
 
-    city = request.GET.get("city", "anywhere")
-    city = str.capitalize(city)
+    city = str.capitalize(request.GET.get("city", "Anywhere"))
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", "0"))
-    room_types = RoomType.objects.all()
+
+    price = request.GET.get('price', 0)
+    guests = request.GET.get('guests', 0)
+    bedrooms = request.GET.get('bedrooms', 0)
+    beds = request.GET.get('beds', 0)
+    baths = request.GET.get('baths', 0)
+    instant = request.GET.get('instant', False)
+    super_host = request.GET.get('super_host', False)
+    s_amenities = request.GET.getlist('amenities', 0)
+    s_facilities = request.GET.getlist('facilities', 0)
 
     form = {
         'city': city,
         's_country': country,
         's_room_type': room_type,
+        'price': price,
+        'guests': guests,
+        'bedrooms': bedrooms,
+        'beds': beds,
+        'baths': baths,
+        'instant': instant,
+        'super_host': super_host,
     }
+
+    room_types = RoomType.objects.all()
+    amentities = Amenity.objects.all()
+    facilities = Facility.objects.all()
 
     choices = {
         "countries": countries,
         'room_types': room_types,
+        'amenities': amentities,
+        'facilities': facilities,
+        's_amenities': s_amenities,
+        's_facilities': s_facilities,
     }
 
     context = {
