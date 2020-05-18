16dc84562f6481acc0f8f138819dbe1217da932e
yykdev <assavictory@gmail.com>
2020-05-18 17:21:16 +0900

    깃허브 소셜 로그인 기능 추가

--- django-app/users/urls.py
+++ django-app/users/urls.py
@@ -6,6 +6,8 @@ app_name = "users"
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),
+    path('login/github/', views.github_login, name="github_login"),
+    path('login/github/callback/', views.github_callback, name="github_callback"),
     path('logout/', views.LogoutView.as_view(), name="logout"),
     path('signup/', views.SignUpView.as_view(), name="signup"),
     path('verify/<str:key>/', views.complete_verification, name="complete_verification"),
--- django-app/users/views.py
+++ django-app/users/views.py
@@ -1,3 +1,6 @@
+import os
+
+import requests
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import View
@@ -5,6 +8,7 @@ from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.views.generic import FormView
 
 from users.forms import LoginForm, SignUpForm
+from utils import Constant
 
 User = get_user_model()
 
@@ -67,3 +71,68 @@ def complete_verification(request, key):
         # to do : add error message
         pass
     return redirect("core:home")
+
+
+def github_login(request):
+    client_id = os.environ.get("GITHUB_CLIENT_ID")
+    redirect_uri = os.environ.get("GITHUB_REDIRECT_URI")
+    return redirect("https://github.com/login/oauth/authorize?client_id={}&redirect_uri={}&scope=read:user".format(client_id, redirect_uri))
+
+
+class GithubException(Exception):
+
+    pass
+
+
+def github_callback(request):
+    try:
+        client_id = os.environ.get("GITHUB_CLIENT_ID")
+        secret_id = os.environ.get("GITHUB_CLIENT_SECRET")
+        code = request.GET.get("code", None)
+        if code is not None:
+            token_requests = requests.post(
+                "https://github.com/login/oauth/access_token?client_id={}&client_secret={}&code={}".format(client_id, secret_id, code),
+                headers={"Accept": "application/json"}
+            )
+            token_json = token_requests.json()
+            error = result_json.get("error", None)
+            if error is not None:
+                raise GithubException()
+            else:
+                access_token = token_json.get("access_token")
+                profile_request = requests.get(
+                    "https://api.github.com/user",
+                    headers={
+                        "Authorization": "token {}".format(access_token)
+                    }
+                )
+                profile_json = profile_request.json()
+                username = profile_json.get("login", None)
+                if username is not None:
+                    name = profile_json.get("name")
+                    email = profile_json.get("email")
+                    bio = profile_json.get("bio")
+
+                    try:
+                        user = User.objects.get(email=email)
+                        if user.login_method != Constant.LOGIN_GITHUB:
+                            raise GithubException()
+                    except User.DoesNotExist:
+                        user = User.objects.create(
+                            email=email,
+                            first_name=name,
+                            username=email,
+                            bio=bio if bio else "",
+                            login_method=Constant.LOGIN_GITHUB,
+                        )
+                        user.set_unusable_password()
+                        user.save()
+                    login(request, user)
+                    return redirect("core:home")
+                else:
+                    raise GithubException()
+        raise GithubException()
+    except Exception as e:
+        print(e)
+        # send error message
+        return redirect("users:login")
