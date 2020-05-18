8c821820db6669e1905d6afb75a14b06955941d8
yykdev <assavictory@gmail.com>
2020-05-18 14:34:41 +0900

    유저 이메일 인증 관련 필드 추가

--- django-app/db/models/users.py
+++ django-app/db/models/users.py
@@ -1,5 +1,11 @@
+import uuid
+
+from django.conf import settings
+from django.core.mail import send_mail
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 from utils import Constant
 
@@ -44,3 +50,26 @@ class User(AbstractUser):
     is_superhost = models.BooleanField(
         default=False,
     )
+    email_verified = models.BooleanField(
+        default=False,
+    )
+    email_secret = models.CharField(
+        max_length=120,
+        default="",
+    )
+
+    def verify_email(self):
+        if self.email_verified is False:
+            secret = uuid.uuid4().hex[:20]
+            self.email_secret = secret
+            html_message = render_to_string('emails/verify_email.html', context={"secret": secret})
+            send_mail(
+                "Verify Airbnb Account",
+                strip_tags(html_message),
+                settings.EMAIL_FROM,
+                [self.email],
+                fail_silently=False,
+                html_message=html_message,
+            )
+            self.save()
+        return
