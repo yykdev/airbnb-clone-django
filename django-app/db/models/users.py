defffe981c09999d0b14af312a73bcb44406137a
yykdev <assavictory@gmail.com>
2020-05-20 01:26:57 +0900

    email_secret 필드 blank True 셋팅

--- django-app/db/models/users.py
+++ django-app/db/models/users.py
@@ -36,7 +36,7 @@ class User(AbstractUser):
     )
     is_superhost = models.BooleanField(default=False,)
     email_verified = models.BooleanField(default=False,)
-    email_secret = models.CharField(max_length=120, default="",)
+    email_secret = models.CharField(max_length=120, default="", blank=True,)
     login_method = models.CharField(
         max_length=50, choices=Constant.LOGIN_CHOICES, default=Constant.LOGIN_EMAIL,
     )
